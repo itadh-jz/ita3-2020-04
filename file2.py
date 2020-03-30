3114d211fbcc8cf7fedb59506e3d3ddcdf08d0b9
Jörg Zimmermann <jz@mgeg.de>
2020-03-30 16:07:55 +0200

    switch variable

---
+++ file2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+zahl1 = 3
+zahl2 = 4
+
+# ergebnis = zahl1 * zahl2
+# print(ergebnis)
+
+print(zahl1*zahl2)
+
---
+++ file3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+zahl1 = 1
+zahl2 = 2
+print(zahl1)
+print(zahl2)
+
+#zahl = zahl1
+#zahl1 = zahl2
+#zahl2 = zahl
+
+#zahl2 = zahl1
+
+zahl1, zahl2 = zahl2, zahl1
+
+print(zahl2,zahl1)
+
+print(zahl1)
+print(zahl2)
