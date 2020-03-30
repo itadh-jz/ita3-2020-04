2bfbaa47bf712c3b6ae7d35e1bfce369271d6173
Jörg Zimmermann <jz@mgeg.de>
2020-03-30 14:31:49 +0200

    first file

---
+++ file1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+ausgabe = "Wert"
+anzahl = "2"
+anzahl = 3
+# michgibtsnicht = "irgendwas"
+
+print("Hallo Welt")
+print(ausgabe*3)
+print(anzahl*3)
+print("anzahl"*3)
+
+print(michgibtsnicht)
+
